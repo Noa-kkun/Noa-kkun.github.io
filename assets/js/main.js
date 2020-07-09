08a417f5c2e4e65a7a0846b522e6197b9ecce020
Noa.kkun <Noakkun@github.com>
2020-07-09 17:57:45 +0530

    stlye modded

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,8 @@
   "use strict";
 
   // Preloader
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
-        $(this).remove();
-      });
-    }
-  });
+  window.onload = function(){ document.getElementById(".lds-hourglass").style.display = "none" }
+
 
   // Hero typed
   if ($('.typed').length) {
